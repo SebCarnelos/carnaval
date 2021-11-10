2b4ab2d358ea78fe7058f035d02b4e15ac418889
SebCarnelos <me@sebcarnelos.fr>
2021-11-10 17:47:40 +0100

    started slider

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
-// Smooth scroll on anchor
 $(document).ready(function() {
-    let image = document.getElementsByClassName('alt-cover');
-    let position = 0;
 
+    //Banner slider
+    // let banner = document.getElementsByClassName('banner');
+    // let banPosition = 0;
+    // const banSlider = () => {
+    //     if (banPosition	 === 1) {
+    //         banner[0].style.position = 'absolute';
+    //         banner[1].style.visibility = 'hidden';
+    //         banPosition --;
+    //     }
+    //     else {
+    //         banner[0].style.position = 'relative';
+    //         banner[1].style.visibility = 'visible';
+    //         banPosition ++;
+    //     }
+    // };
+    // setInterval(banSlider, 1000)
+
+
+    // Alternate Covers
+    let image = document.getElementsByClassName('alt-cover');
+    let imgPosition = 0;
     const changeImage = () => {
-        if (position === 1) {
+        if (imgPosition === 1) {
             image[0].style.display = 'inline';
             image[1].style.display = 'none';
-            position --;
+            imgPosition --;
         }
         else {
             image[0].style.display = 'none';
             image[1].style.display = 'inline';
-            position ++;
+            imgPosition ++;
         }
     };
-
     setInterval(changeImage, 3000)
 });
