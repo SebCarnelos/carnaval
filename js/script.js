55e7f1f485a9005217538494aad83f2ab388791f
SebCarnelos <me@sebcarnelos.fr>
2022-04-03 02:48:40 +0200

    added Lost Angels anniversary banner

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@ document.addEventListener('DOMContentLoaded', function(){
       
       
     //Banner slider
-    const bannerArray = document.querySelectorAll('#dark-gradient > .container');
+    const bannerArray = document.querySelectorAll('#gradient > .container');
     const bgImage = document.getElementById('slider');
-    const gradient = 'linear-gradient(79.19deg, #00080A 0%, rgba(0, 8, 10, 0) 86.22%), ';
-    const imgages = ['url("img/lady_wolf_ban.jpg")', 'url("img/generation_heros_ban.jpg")'];
+    const gradient = document.getElementById('gradient');
+    const imgages = ['url("img/lady_wolf_ban.jpg")', 'url("img/generation_heros_ban.jpg")', 'url("img/lost_angels_ban.jpg")'];
     bgImage.style.backgroundImage = imgages[0];
     bgImage.style.backgroundPosition = 'right';
     let slidePosition = 0;
-    bannerArray[1].style.display = 'none';
-    bannerArray[1].style.opacity = 0;
+    bannerArray[1].style.display = bannerArray[2].style.display = 'none';
+    bannerArray[1].style.opacity = bannerArray[2].style.opacity = 0;
 
     const changeSlide = () => {
-        if (slidePosition === 1) {
-            // bgImage.style.background = gradient + imgages[0];
+        if (slidePosition === 2) {
             bgImage.style.backgroundImage = imgages[0];
             bgImage.style.backgroundPosition = 'right';
-            bannerArray[1].style.opacity = 0;
+            bannerArray[2].style.opacity = 0;
+            gradient.classList.add('dark-gradient');
+            gradient.classList.remove('white-gradient');
             setTimeout(() => {
-                bannerArray[1].style.display = 'none';
+                bannerArray[2].style.display = 'none';
                 bannerArray[0].style.display = 'flex';
             }, 500);
             setTimeout(() => {bannerArray[0].style.opacity = 1;}, 600);
-            slidePosition --;
-        } else {
-            // bgImage.style.background = gradient + imgages[1];
+            slidePosition = 0;
+        } else if (slidePosition === 0) {
             bgImage.style.backgroundImage = imgages[1];
             bgImage.style.backgroundPosition = 'left';
             bannerArray[0].style.opacity = 0;
@@ -34,7 +34,19 @@ document.addEventListener('DOMContentLoaded', function(){
                 bannerArray[0].style.display = 'none';
                 bannerArray[1].style.opacity = 1;
             }, 500);
-            slidePosition ++;
+            slidePosition = 1;
+        } else { // if (slidePosition === 1)
+            bgImage.style.backgroundImage = imgages[2];
+            bgImage.style.backgroundPosition = 'center';
+            bannerArray[1].style.opacity = 0;
+            bannerArray[2].style.display = 'flex';
+            gradient.classList.remove('dark-gradient');
+            gradient.classList.add('white-gradient');
+            setTimeout(() => {
+                bannerArray[1].style.display = 'none';
+                bannerArray[2].style.opacity = 1;
+            }, 500);
+            slidePosition = 2;
         }
      };
     setInterval(changeSlide, 6000);
