e5b77fd16f56b1140aa009d78917a9be2562b1c2
Seb Carnélos <SebCarnelos@users.noreply.github.com>
2026-06-17 02:15:47 +0200

    New website WIP

--- js/script.js
+++ js/script.js
@@ -1,69 +1,7 @@
-document.addEventListener('DOMContentLoaded', function(){
-      
-      
-    //Banner slider
-    const bannerArray = document.querySelectorAll('#gradient > .container');
-    const bgImage = document.getElementById('slider');
-    const gradient = document.getElementById('gradient');
-    const images = ['url("img/lady_wolf_ban.jpg")', 'url("img/generation_heros_ban.jpg")', 'url("img/lost_angels_ban.jpg")'];
-    bgImage.style.backgroundImage = images[0];
-    bgImage.style.backgroundPosition = 'left';
-    let slidePosition = 0;
-    bannerArray[1].style.display = bannerArray[2].style.display = 'none';
-    bannerArray[1].style.opacity = bannerArray[2].style.opacity = 0;
-
-    const changeSlide = () => {
-        if (slidePosition === 2) {
-            bgImage.style.backgroundImage = images[0]; // LW
-            bgImage.style.backgroundPosition = 'left';
-            bannerArray[2].style.opacity = 0;
-            gradient.classList.add('dark-gradient');
-            gradient.classList.remove('white-gradient');
-            setTimeout(() => {
-                bannerArray[2].style.display = 'none';
-                bannerArray[0].style.display = 'flex';
-            }, 500);
-            setTimeout(() => {bannerArray[0].style.opacity = 1;}, 600);
-            slidePosition = 0;
-        } else if (slidePosition === 0) {
-            bgImage.style.backgroundImage = images[1]; // GH
-            bgImage.style.backgroundPosition = 'center';
-            bannerArray[0].style.opacity = 0;
-            bannerArray[1].style.display = 'flex';
-            setTimeout(() => {
-                bannerArray[0].style.display = 'none';
-                bannerArray[1].style.opacity = 1;
-            }, 500);
-            slidePosition = 1;
-        } else { // if (slidePosition === 1)
-            bgImage.style.backgroundImage = images[2]; // LA
-            bgImage.style.backgroundPosition = 'right';
-            bannerArray[1].style.opacity = 0;
-            bannerArray[2].style.display = 'flex';
-            gradient.classList.remove('dark-gradient');
-            gradient.classList.add('white-gradient');
-            setTimeout(() => {
-                bannerArray[1].style.display = 'none';
-                bannerArray[2].style.opacity = 1;
-            }, 500);
-            slidePosition = 2;
-        }
-     };
-    setInterval(changeSlide, 6000);
-
-    // Alternate Covers
-    const image = document.getElementsByClassName('alt-cover');
-    let imgPosition = 0;
-    const changeImage = () => {
-        if (imgPosition === 1) {
-            image[0].style.display = 'inline';
-            image[1].style.display = 'none';
-            imgPosition --;
-        } else {
-            image[0].style.display = 'none';
-            image[1].style.display = 'inline';
-            imgPosition ++;
-        }
-    };
-    setInterval(changeImage, 3000);
-});
+<!-- MailerLite Universal -->
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '1309442');
+<!-- End MailerLite Universal -->
